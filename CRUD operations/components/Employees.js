68572e2d5f85c5bd900b0e809b20e17f516e3776
Asrarul Hoque Eusha <106666923+Asrarul-Hoque-Eusha@users.noreply.github.com>
2024-03-21 17:36:23 +0600

    Add files via upload

---
+++ CRUD operations/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Rahat",
+    Age: "23",
+  },
+  {
+    id: "2",
+    Name: "Mahin",
+    Age: "25",
+  },
+  {
+    id: "3",
+    Name: "Eusha",
+    Age: "24",
+  },
+];
+export default Employees;
